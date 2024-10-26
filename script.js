4118da1be3a760e81b0af54007a31b7998d6b129
Vilislav <vilislavangelov@gmail.com>
2024-10-26 17:52:02 +0300

    Did all the mandatory tasks for this project
    Used outline instead of border and worked much
    better.
    fixed an issue where you couldn't draw because of
    dragging.
    
    onto the bonus points now

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//constraints
-//remove the ability to drag on the grid and instead make it paint
-
 //BONUS
 //randomize color on each hover
 //Additionally, implement a progressive darkening effect where each interaction darkens the square by 10%. The goal is to achieve a fully black (or completely colored) square in only ten interactions. OPACITY.
@@ -12,56 +9,53 @@ let gridNum = 16;
 sizeGrid(gridNum);
 
 function askSize() {
+  gridNum = prompt("What size grid do you want?");
+  while (gridNum > 100 || gridNum <= 1) {
+    alert("Grid Size Should Be Between 2 and 100");
     gridNum = prompt("What size grid do you want?");
-        while(gridNum > 100) {
-            alert("Grid Size Should Be Less Than 100");
-            gridNum = prompt("What size grid do you want?");
-        }
+  }
 
-    clearGrid();
-    sizeGrid(gridNum);
+  clearGrid();
+  sizeGrid(gridNum);
 }
 
 function sizeGrid(gridNum) {
-    for (let i = 0; i < gridNum; i++){
-        let gridCol = document.createElement('div')
-        gridCol.classList.add("gridCol");
-        sketchContainer.appendChild(gridCol);
-        for(let j = 0; j < gridNum; j++) {
-            let gridBox = document.createElement('div')
-            gridBox.classList.add("gridBox");
-            gridCol.appendChild(gridBox);
-        }
+  for (let i = 0; i < gridNum; i++) {
+    let gridCol = document.createElement("div");
+    gridCol.classList.add("gridCol");
+    sketchContainer.appendChild(gridCol);
+    for (let j = 0; j < gridNum; j++) {
+      let gridBox = document.createElement("div");
+      gridBox.classList.add("gridBox");
+      gridCol.appendChild(gridBox);
     }
+  }
 
-    paint();
+  paint();
 }
 
-console.log("hello");
-
-
 function clearGrid() {
-    document.getElementById("sketchContainer").innerHTML = "";
+  document.getElementById("sketchContainer").innerHTML = "";
 }
 
 function paint() {
-    const gridBoxes = document.querySelectorAll(".gridBox");
+  const gridBoxes = document.querySelectorAll(".gridBox");
 
-    gridBoxes.forEach(box => {
-        box.addEventListener("mousedown", () => {
-            box.style.backgroundColor = "black";
-    
-            const paint = (e) => {
-                if (e.target.classList.contains("gridBox")) {
-                    e.target.style.backgroundColor = "black";
-                }
-            };
-    
-            document.addEventListener("mousemove", paint);
-            
-            document.addEventListener("mouseup", () => {
-                document.removeEventListener("mousemove", paint);
-            });
-        });
+  gridBoxes.forEach((box) => {
+    box.addEventListener("mousedown", () => {
+      box.style.backgroundColor = "black";
+
+      const paint = (e) => {
+        if (e.target.classList.contains("gridBox")) {
+          e.target.style.backgroundColor = "black";
+        }
+      };
+
+      document.addEventListener("mousemove", paint);
+
+      document.addEventListener("mouseup", () => {
+        document.removeEventListener("mousemove", paint);
+      });
     });
+  });
 }
